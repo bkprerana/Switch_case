21d9f130c1f07709cbfeda4074b4169806b4c89e
bkprerana <125630134+bkprerana@users.noreply.github.com>
2023-04-05 10:14:19 +0000

    first commit

---
+++ file.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int i,digit;
+    printf("input digits(0-6):");
+    scanf("%d",&digit);
+    switch(digit)
+    {
+        case 0:
+        printf("zero");
+        break;
+
+        case 1:
+        printf("one");
+        break;
+
+        case 2:
+        printf("two");
+        break;
+
+        case 3:
+        printf("three");
+        break;
+
+        case4:
+        printf("four");
+        break;
+
+        case 5:
+        printf("five");
+        break;
+
+        case 6:
+        printf("six");
+        break;
+
+    
+    }
+    return 0;
+}
